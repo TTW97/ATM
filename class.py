82d2068ec40f7755047e946347738f24bcc41691
TTW97 <68407647+TTW97@users.noreply.github.com>
2021-05-31 12:56:05 -0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class Atm:
+    def __init__(self, card_number, pin_number):
+        self.name = name
+        self.card_number = card_number
+        self.pin_number = pin_number
+
+    def setMoney(self, pin_number, money_have):
+        self.pin_number[money_have] = money_have
+
+    def getMoney(self, pin_number):
+        return self.money_have[pin_number]
+    
+Card = Atm(22233, 4455)
